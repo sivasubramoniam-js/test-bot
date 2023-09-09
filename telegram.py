cf5274821bf7468eb869833ba97e314b6222e0c2
sivasubramoniam-js <122598293+sivasubramoniam-js@users.noreply.github.com>
2023-09-09 15:01:04 +0530

    summary and description

--- telegram.py
+++ telegram.py
@@ -2,7 +2,7 @@ import subprocess
 
 try:
     print('installing Dependencies.')
-    subprocess.check_call(['pip', 'install', '-r', 'requirements.txt'])
+    subprocess.check_call(['pip3', 'install', '-r', 'requirements.txt'])
     print('Dependencies installed successfully.')
 except subprocess.CalledProcessError:
     print('Failed to install dependencies.')
@@ -11,6 +11,10 @@ import telebot
 import ssl
 import os
 from dotenv import load_dotenv
+from gradio_client import Client
+from wiki import search
+
+client = Client("http://47.103.63.15:50085/")
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -25,7 +29,34 @@ bot = telebot.TeleBot(BOT_TOKEN)
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
     print(f'Message from user : {message.from_user.first_name}')
-    bot.reply_to(message, f'Hey, how are you doing {message.from_user.first_name} from @codewithjss?')
+    bot.reply_to(message, f'Hey, how are you doing {message.from_user.first_name} - from @codewithjss?')
+
+@bot.message_handler(commands=['chat'])
+def process_req(message):
+    print(f'Message from user : {message.from_user.first_name}')
+    filtered_text = message.text.removeprefix('/chat ')
+    result = client.predict(
+				filtered_text,	# str in 'Instruction' Textbox component
+				1,
+                2048,
+				api_name="/predict"
+    )
+    bot.reply_to(message, result)
+
+@bot.message_handler(commands=['summary'])
+def get_summary(message):
+    print(f'Message from user : {message.from_user.first_name}')
+    filtered_text = message.text.removeprefix('/summary ')
+    result = search('summary', filtered_text)
+    bot.reply_to(message, result)
+
+
+@bot.message_handler(commands=['description'])
+def get_desc(message):
+    print(f'Message from user : {message.from_user.first_name}')
+    filtered_text = message.text.removeprefix('/description ')
+    result = search('description', filtered_text)
+    bot.reply_to(message, result)
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
---
+++ wiki.py
@@ -0,0 +1,31 @@
+import wikipedia
+
+# Set the language of wikipedia you want to use (default is English)
+
+# Specify the search query
+
+def search(type,search_query):
+    if type == 'summary':
+        try:
+            # Get the summary of the page
+            summary = wikipedia.summary(search_query)
+            return(f"**Summary of '{search_query}':\n{summary}\n**")
+
+        except wikipedia.exceptions.PageError:
+            return(f"'{search_query}' page not found on wikipedia.")
+
+        except wikipedia.exceptions.DisambiguationError as e:
+            # Handle disambiguation pages
+            return(f"'{search_query}' may refer to multiple topics. Options:\n{e.options}")
+    else:
+        try:
+            # Get the full text of the page
+            full_text = wikipedia.page(search_query).content
+            return(f"**Description of '{search_query}':\n{full_text}**")
+
+        except wikipedia.exceptions.PageError:
+            return(f"'{search_query}' page not found on wikipedia.")
+
+        except wikipedia.exceptions.DisambiguationError as e:
+            # Handle disambiguation pages
+            return(f"'{search_query}' may refer to multiple topics. Options:\n{e.options}")
