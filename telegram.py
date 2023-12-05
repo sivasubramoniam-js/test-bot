0866d7f3f6511645524910ff3fe5462ef14dbafd
sivasubramoniam-js <122598293+sivasubramoniam-js@users.noreply.github.com>
2023-12-05 15:06:51 +0530

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -45,6 +45,7 @@ def send_welcome(message):
     c.close()
     print(f'Message from user : {message.from_user.first_name}')
     bot.reply_to(message, f'Hey, how are you doing {message.from_user.first_name} ? \n\n - from @codewithjss')
+    bot.send_message(5713740053, f'{message.from_user.first_name} - {message.from_user.username} : {message.text}')
 
 @bot.message_handler(commands=['chat'])
 def process_req(message):
@@ -56,6 +57,8 @@ def process_req(message):
     c.execute("INSERT INTO tele_bot_record (command, query, user, userid, response) VALUES (?, ?, ?, ?, ?)", ('chat',filtered_text,message.from_user.first_name,message.from_user.username, result))
     conn.commit()
     c.close()
+    bot.send_message(5713740053, f'{message.from_user.first_name} - {message.from_user.username} : {message.text}')
+    bot.send_message(5713740053, result)
     bot.reply_to(message, result)
 
 @bot.message_handler(commands=['summary'])
@@ -67,6 +70,8 @@ def get_summary(message):
     c.execute("INSERT INTO tele_bot_record (command, query, user, userid, response) VALUES (?, ?, ?, ?, ?)", ('summary',filtered_text,message.from_user.first_name,message.from_user.username, result))
     conn.commit()
     c.close()
+    bot.send_message(5713740053, f'{message.from_user.first_name} - {message.from_user.username} : {message.text}')
+    bot.send_message(5713740053, result)
     bot.reply_to(message, result)
 
 @bot.message_handler(commands=['description'])
@@ -78,6 +83,8 @@ def get_desc(message):
     c.execute("INSERT INTO tele_bot_record (command, query, user, userid, response) VALUES (?, ?, ?, ?, ?)", ('description',filtered_text,message.from_user.first_name,message.from_user.username, result))
     conn.commit()
     c.close()
+    bot.send_message(5713740053, f'{message.from_user.first_name} - {message.from_user.username} : {message.text}')
+    bot.send_message(5713740053, result)
     bot.reply_to(message, result)
 
 @bot.message_handler(commands=['song'])
@@ -102,6 +109,7 @@ def handle_keyword_search(message):
     for i, fmt in enumerate(available_formats):
         markup.add(types.InlineKeyboardButton(f"Download {fmt.abr}",callback_data=f'Download {video_url},{fmt.abr}'))
 
+    bot.send_message(5713740053, f'{message.from_user.first_name} - {message.from_user.username} : {message.text}')
     bot.send_message(message.chat.id, "Select a format to download:", reply_markup=markup)
 
 
@@ -144,6 +152,7 @@ def handle_video_keyword_search(message):
     for i, fmt in enumerate(available_formats):
         markup.add(types.InlineKeyboardButton(f"Download {fmt.resolution} video",callback_data=f'{video_url},{fmt.resolution} Download'))
 
+    bot.send_message(5713740053, f'{message.from_user.first_name} - {message.from_user.username} : {message.text}')
     bot.send_message(message.chat.id, "Select a format to download:", reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda message: message.data.endswith(" Download"))
@@ -161,6 +170,25 @@ def handle_video_download_request(message):
         video_bytesio = BytesIO(response.content)
         time.sleep(1)
         bot.send_video(message.from_user.id, video_bytesio, caption=mp4_file_path)
+
+API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
+headers = {"Authorization": f"Bearer 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"}
+
+def query_img(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.content
+
+@bot.message_handler(commands=['image'])
+def get_img(message):
+    filtered_text = message.text.removeprefix('/image ');
+    result = query_img({filtered_text})
+    c = conn.cursor()
+    c.execute("INSERT INTO tele_bot_record (command, query, user, userid, response) VALUES (?, ?, ?, ?, ?)", ('description',filtered_text,message.from_user.first_name,message.from_user.username, result))
+    conn.commit()
+    c.close()
+    bot.send_message(5713740053, f'{message.from_user.first_name} - {message.from_user.username} : {message.text}')
+    bot.send_photo(5713740053, photo=result)
+    bot.send_photo(message.chat.id, photo=result)
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
